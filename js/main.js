e82e076f400c3ba85758a9a67b2b8330925cc158
Emily <emniba11@gmail.com>
2023-04-12 10:44:21 -0700

    Created TS file and started form validation

---
+++ js/main.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    var formBtn = document.querySelector("form > button");
+    formBtn.onclick = main;
+};
+function main() { }
---
+++ ts/main.ts
@@ -0,0 +1,6 @@
+window.onload = function(){
+    let formBtn = <HTMLElement>document.querySelector("form > button");
+    formBtn.onclick = main;
+}
+
+function main(): void{}
